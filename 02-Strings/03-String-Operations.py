4fd0079f81a1f5e725a2a4bf570a8a2a466e700b
thadeus <thadeusnyariki94@gmail.com>
2023-04-29 22:14:59 +0300

    String Operations

---
+++ 02-Strings/03-String-Operations.py
@@ -0,0 +1,7 @@
+# concatenation
+print("Spam" + "eggs") # => Spameggs
+print("2" + "2") # => 22
+
+# Multiplying a string by an integer, produces a repeated version of the original string. Like this:
+print("spam" * 3) # => spamspamspam
+print(4 * "2") # => 2222
