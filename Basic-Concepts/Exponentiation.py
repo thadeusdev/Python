a2a1820021a3abb4e19ab6210cd45dd029b828ee
thadeus <thadeusnyariki94@gmail.com>
2023-04-29 21:03:25 +0300

    Exponentiation

---
+++ Basic-Concepts/Exponentiation.py
@@ -0,0 +1,5 @@
+print(2**5) # => 32
+
+# You can chain exponentiations together. In other words, you can rise a number to multiple powers. For example, 2**3**2.
+print(2**3**2) # => 512
+print(9**(1/2)) # => 3.0
